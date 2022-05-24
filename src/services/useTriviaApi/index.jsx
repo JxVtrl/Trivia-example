398bdd1b9ad746cd734cfbb9846dbd8cef124e38
Joao <joaoviniciusvitral@hotmail.com>
2022-05-24 12:44:43 -0300

    fix: utils import fix

--- src/services/useTriviaApi/index.jsx
+++ src/services/useTriviaApi/index.jsx
@@ -1,4 +1,4 @@
-import { options } from '../useTriviaOptions'
+import { options } from '../../utils'
 export function createURL(amount, category, difficulty) {
     const categoryKey = Object.keys(options.themes).find(key => options.themes[key] === category)
     console.log(`https://opentdb.com/api.php?amount=${amount}&category=${categoryKey}&difficulty=${difficulty}&type=multiple`)
