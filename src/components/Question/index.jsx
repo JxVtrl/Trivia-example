d75e082d6b7c70fd8736c2d778973c9e06628253
Joao <joaoviniciusvitral@hotmail.com>
2022-05-24 09:44:22 -0300

    feat: question updates

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,25 +1,54 @@
 import React from 'react';
 import { useApp } from '../../context';
-// import { Container } from './styles';
+import { useDevice } from '../../hooks';
+import { Container } from './styles';
 
 export function Question({ question, answers, correct }) {
     const { setQuestionNum, questionNum } = useApp();
+    const { device: { isMobile }} = useDevice();
     answers = answers.split('%20');
+    answers.sort(() => Math.random() - 0.5);
 
     function createMarkup(question) {
         return { __html: question };
     }
 
+    function verifyAnswer(event) {
+        if (event.target.innerText === correct) {
+            event.target.classList.add('correct');
+            setTimeout(() => {
+                setQuestionNum(questionNum + 1);
+            }, 2200)
+        } else {
+            event.target.classList.add('wrong');
+
+            document.querySelectorAll('.list')[questionNum].childNodes.forEach(item => {
+                if (item.innerText === correct) {
+                    item.classList.add('correct');
+                    setTimeout(() => {
+                        item.classList.remove('correct');
+                    }, 2200)
+                }
+            })
+
+            setTimeout(() => {
+                setQuestionNum(questionNum + 1);
+            }, 2200)
+        }
+    }
+
     return (
-        <div>
+        <Container mobile={isMobile}>
             <h1 dangerouslySetInnerHTML={createMarkup(question)} />
-            <div>
-                <ul>
-                    {answers.map((item, index) => (
-                        <li key={index} dangerouslySetInnerHTML={createMarkup((item))} />
-                    ))}
-                </ul>
-            </div>
-        </div>
+            <ul className='list' >
+                {answers.map((item, index) => (
+                    <li
+                        onClick={e => verifyAnswer(e)}
+                        key={index}
+                        dangerouslySetInnerHTML={createMarkup((item))}
+                    />
+                ))}
+            </ul>
+        </Container>
   );
 }
---
+++ src/components/Question/styles.js
@@ -0,0 +1,75 @@
+import styled, { css, keyframes } from 'styled-components';
+
+export const correct = keyframes`
+    from {
+        background-color: #ccc;
+    }
+    to {
+        background-color: #00ff00;
+    } 
+`
+
+export const wrong = keyframes`
+    from {
+        background-color: #ccc;
+    }
+    to {
+        background-color: #ff0000;
+    } 
+`
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    margin: 20px 0 0;
+
+    h1 {
+        font-size: ${props => props.mobile ? '1rem' : '1.4rem'};
+        text-align: center;
+        margin: 0 20px;
+    }
+  
+    ul {
+        margin: 20px 0;
+
+        ${props => props.mobile ? css`
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 20px;
+        ` : css`
+            display: grid;
+            grid-template-columns: ${props => props.mobile ? '1fr' : 'repeat(2, 1fr)'};
+            grid-gap: 20px;
+            place-items: center;
+        `}
+    }
+
+    li {
+        list-style: none;
+        
+        width: 220px;
+        height: 100px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        cursor: pointer;
+
+        border: 1px solid #ccc;
+        transition: all 0.3s;
+
+        &:hover {
+            background: #ccc;
+        }
+    }
+
+    li.correct {
+        animation: ${correct} 0.3s infinite;
+    }
+
+    li.wrong {
+        animation: ${wrong} 0.3s infinite;
+    }
+
+`;
